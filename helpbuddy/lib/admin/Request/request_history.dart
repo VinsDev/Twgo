a09c1b1f14f5385c1a693bde6e2bb8e5cf1e6f06
VinsDev <vinsdev185@gmail.com>
2023-05-22 16:01:53 +0100

    api client

--- helpbuddy/lib/admin/Request/request_history.dart
+++ helpbuddy/lib/admin/Request/request_history.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:helpbuddy/widget/button.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class RequestHistory extends StatefulWidget {
   const RequestHistory({Key? key}) : super(key: key);
@@ -26,33 +24,122 @@ class _RequestHistoryState extends State<RequestHistory> {
               size: 16,
             ),
           ),
-          title: Text('Request History',
+          title: Text('History',
               style: GoogleFonts.urbanist(
                   fontWeight: FontWeight.w700,
-                  fontSize: 14,
+                  fontSize: 18,
                   color: Colors.black)),
           centerTitle: true,
+          elevation: 0,
         ),
-        body: ListView.builder(
-            padding: EdgeInsets.only(top: 20),
-            itemCount: 10,
-            itemBuilder: (BuildContext context, int int) {
-              return const RequestCard(data: 'abc@hotmail.com');
-            }));
+        body: Column(
+          children: [
+            Container(
+              color: Colors.white,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text('Any status',
+                          style: GoogleFonts.urbanist(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 14,
+                              color: Color.fromARGB(255, 104, 104, 104))),
+                      const SizedBox(
+                        width: 2,
+                      ),
+                      const Icon(Icons.arrow_drop_down),
+                      const SizedBox(
+                        width: 12,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+                child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text('31/12/2022 - 01/03/2023',
+                          style: GoogleFonts.urbanist(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 14,
+                              color: Color.fromARGB(255, 0, 0, 0))),
+                      const SizedBox(
+                        width: 2,
+                      ),
+                      const Icon(Icons.arrow_drop_down),
+                      const SizedBox(
+                        width: 12,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 4,
+                  ),
+                  Row(
+                    children: [
+                      Text('In: ',
+                          style: GoogleFonts.urbanist(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 14,
+                              color: Color.fromARGB(255, 0, 0, 0))),
+                      Text('£240,000.10',
+                          style: GoogleFonts.urbanist(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 14,
+                              color: Color.fromARGB(255, 102, 102, 102))),
+                      Expanded(child: Container()),
+                      Text('Out: ',
+                          style: GoogleFonts.urbanist(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 14,
+                              color: Color.fromARGB(255, 0, 0, 0))),
+                      Text('£240,000.10',
+                          style: GoogleFonts.urbanist(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 14,
+                              color: Color.fromARGB(255, 102, 102, 102))),
+                    ],
+                  ),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: const [
+                          RequestCard(),
+                          RequestCard(),
+                          RequestCard(),
+                          RequestCard(),
+                          RequestCard(),
+                          RequestCard(),
+                          RequestCard(),
+                          RequestCard(),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )),
+          ],
+        ));
   }
 }
 
 class RequestCard extends StatelessWidget {
-  const RequestCard({Key? key, required this.data}) : super(key: key);
-  // QueryDocumentSnapshot<Object?>? data;
-  final String data;
+  const RequestCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(vertical: 10),
-      padding: EdgeInsets.only(right: 20, top: 10),
-      decoration: BoxDecoration(
+      padding: const EdgeInsets.all(15),
+      decoration: const BoxDecoration(
         color: Colors.white,
         boxShadow: [
           BoxShadow(
@@ -62,53 +149,68 @@ class RequestCard extends StatelessWidget {
           ),
         ],
       ),
-      height: 90,
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Row(
+          Icon(Icons.arrow_drop_down),
+          Image.asset(
+            "assets/images/Twgo 1.png",
+            width: 50,
+            height: 50,
+          ),
+          SizedBox(
+            width: 2,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(width: 5, height: 90, color: Colors.green),
-              const SizedBox(width: 10),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(data,
-                      style: GoogleFonts.urbanist(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 14,
-                          color: Colors.black)),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text('Request History',
-                      style: GoogleFonts.urbanist(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 12,
-                          color: const Color(0xff686868))),
-                ],
-              )
+              Text('You started a project',
+                  style: GoogleFonts.urbanist(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 12,
+                      color: Colors.black)),
+              const SizedBox(
+                height: 2,
+              ),
+              Text('Twgo Wallet Balance',
+                  style: GoogleFonts.urbanist(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 11,
+                      color: Color.fromARGB(255, 141, 141, 141))),
+              const SizedBox(
+                height: 2,
+              ),
+              Text('Status',
+                  style: GoogleFonts.urbanist(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 13,
+                      color: const Color(0xff686868))),
             ],
           ),
           Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
             children: [
-              Container(
-                decoration: const BoxDecoration(color: Color(0xffD9FFEF)),
-                child: Padding(
-                  padding: const EdgeInsets.all(5.0),
-                  child: Text('Complete',
-                      style: GoogleFonts.urbanist(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 14,
-                          color: const Color(0xff006C3F))),
-                ),
-              ),
+              Text('£1000',
+                  style: GoogleFonts.urbanist(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16,
+                      color: const Color(0xff006C3F))),
               const SizedBox(
-                height: 8,
+                height: 2,
               ),
-              const Text('D  H  M  S'),
-              const Text('27:15:09:02'),
+              Text('Mar 01 15:30',
+                  style: GoogleFonts.urbanist(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 12,
+                      color: Color.fromARGB(255, 102, 102, 102))),
+              const SizedBox(
+                height: 2,
+              ),
+              Text('Transaction successful',
+                  style: GoogleFonts.urbanist(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 12,
+                      color: Color.fromARGB(255, 0, 199, 50))),
             ],
           ),
         ],
